f6222ca8ae90d9b68d701893380edd19c489be56
JARodenArt <jarodenart@gmail.com>
2024-06-07 22:13:54 -0400

    Trying Web3Forms

--- docs/myscripts.js
+++ docs/myscripts.js
@@ -9,19 +9,41 @@ function myFunction() {
 
   }
 
-function sendMail(){
-  let params = {
-    from_name : document.getElementById("from_name").value,
-    subject : document.getElementById("subject").value,
-    message : document.getElementById("message").value,
-  }
-
-  emailjs.send("service_pc36k5i","template_bfyie4a", params).then(function (res){
-    alert("Success! " + res.status);
-  })
-}
-
-const form =
-document.getElementById("myform");
-form.addEventListener("submit", sendMail);
+  const form = document.getElementById('form');
+  const result = document.getElementById('result');
   
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+  
+      fetch('https://api.web3forms.com/submit', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              },
+              body: json
+          })
+          .then(async (response) => {
+              let json = await response.json();
+              if (response.status == 200) {
+                  result.innerHTML = "Form submitted successfully";
+              } else {
+                  console.log(response);
+                  result.innerHTML = json.message;
+              }
+          })
+          .catch(error => {
+              console.log(error);
+              result.innerHTML = "Something went wrong!";
+          })
+          .then(function() {
+              form.reset();
+              setTimeout(() => {
+                  result.style.display = "none";
+              }, 3000);
+          });
+  });
